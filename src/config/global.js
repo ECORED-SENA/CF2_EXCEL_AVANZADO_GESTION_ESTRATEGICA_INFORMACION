53bcfe1d0c2d22aec49ea26011d1f81a10b0ca08
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-09-29 19:33:08 -0500

    CF ajustado

--- src/config/global.js
+++ src/config/global.js
@@ -181,21 +181,21 @@ export default {
       tema: 'Macros',
       referencia: 'Deivis Eduard Ramirez Martinez (2025). Dataset empleados. ',
       tipo: 'Archivo Excel',
-      link: '../downloads/empleados.csv',
+      link: './downloads/empleados.csv',
     },
     {
       tema: 'Macros',
       referencia:
         'Deivis Eduard Ramirez Martinez (2025). Dataset empleados_ejemplo. ',
       tipo: 'Archivo Excel',
-      link: '../downloads/empleados_ejemplo.csv',
+      link: './downloads/empleados_ejemplo.csv',
     },
     {
       tema: 'Macros',
       referencia:
         'Deivis Eduard Ramirez Martinez (2025). Dataset Practica interfaz. ',
       tipo: 'Archivo Excel ',
-      link: '../downloads/PracticaInterfaz.xlsm',
+      link: './downloads/PracticaInterfaz.xlsm',
     },
   ],
   glosario: [
