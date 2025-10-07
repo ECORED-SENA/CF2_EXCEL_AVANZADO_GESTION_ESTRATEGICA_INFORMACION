b5ec83235fe60d454bfb12c5413276c7eb398c83
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-10-07 15:36:58 -0500

    Vinculación PDF - MD

--- src/config/global.js
+++ src/config/global.js
@@ -141,7 +141,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/Excel_Avanzado_GEI_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
