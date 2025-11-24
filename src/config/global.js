0f83906f3225bf04b072021bad1dde43a08ab45d
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2025-11-24 11:33:06 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -141,7 +141,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/Excel_Avanzado_GEI_CF02_DU.pdf',
+        download: 'downloads/Excel_Avanzado_GEI_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
